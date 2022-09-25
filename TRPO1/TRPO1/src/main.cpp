a03a1369f65fdcb74f756db783e3f00ddd506780
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-09-25 11:37:35 +0300

    More small fixes. Ran code through formatter.

--- TRPO1/TRPO1/src/main.cpp
+++ TRPO1/TRPO1/src/main.cpp
@@ -16,81 +16,74 @@ const std::string Output_FName("Output_File.txt");
 
 //функция считывания массива из файла
 void Get_Array(const std::string_view Input_FName, std::vector<int>& array, int& Err_Code) {
-	
-	std::ifstream fin(Input_FName.data());
-	int test_numb = 0;
-	//файл не найден
-	if (!fin)
-	{
-		Err_Code = 1;
-		return;
-	}//if
+    std::ifstream fin(Input_FName.data());
+    int test_numb = 0;
+    //файл не найден
+    if (!fin) {
+        Err_Code = 1;
+        return;
+    }//if
 
-	if (fin.peek() == EOF) //файл пуст
-	{
-		Err_Code = 2;
-		return;
-	}//if
+    if (fin.peek() == EOF) //файл пуст
+    {
+        Err_Code = 2;
+        return;
+    }//if
 
-	fin >> test_numb;
-	if (fin.fail())  //в файле не число
-	{
-		Err_Code = 3;
-		return;
-	}
+    fin >> test_numb;
+    if (fin.fail())  //в файле не число
+    {
+        Err_Code = 3;
+        return;
+    }
 
-	array.push_back(test_numb);
+    array.push_back(test_numb);
 
-	while (!fin.eof()) //пока файл не кончился
-	{	
-		fin >> test_numb;
-		array.push_back(test_numb);
-	}//while
+    while (!fin.eof()) //пока файл не кончился
+    {
+        fin >> test_numb;
+        array.push_back(test_numb);
+    }//while
 
-	fin.close();
-
+    fin.close();
 }//Get_Array()
 
 //функция сортировки пузырьком
 void Bubble_Sort(std::vector<int>& array) {
-	for (size_t i = 0; i+1 < array.size(); i++) {
-		for (size_t j = 0; j+1 < array.size() - i; j++) {
-			if (array[j+1] < array[j]) {
-				std::swap(array[j], array[j+1]);
-			}//if
-		}//for j
-	}//for i
-
+    for (size_t i = 0; i + 1 < array.size(); i++) {
+        for (size_t j = 0; j + 1 < array.size() - i; j++) {
+            if (array[j + 1] < array[j]) {
+                std::swap(array[j], array[j + 1]);
+            }//if
+        }//for j
+    }//for i
 }//Bubble_Sort()
 
 //функция печати массива в файл
 void Print_Array(const std::string_view Output_FName, std::vector<int>& array) {
+    int i = 0;
 
-	int i = 0;
+    std::ofstream output(Output_FName.data());
+    if (array.size() == 0) {
+        output.close();
+        return;
+    }
 
-	std::ofstream output(Output_FName.data());
-	if (array.size() == 0) {
-		output.close();
-		return;
-	}
-
-	for (i = 0; i < array.size() - 1; i++) {
-		output << array.at(i) << ' ';
-	}
-	output << array.at(i) << std::endl;
-	output.close();
-	return;
-
+    for (i = 0; i < array.size() - 1; i++) {
+        output << array.at(i) << ' ';
+    }
+    output << array.at(i) << std::endl;
+    output.close();
+    return;
 }//Print_Array()
 
-
 int main() {
-	system("color F0");
-	setlocale(LC_ALL, "russian");
-	std::vector<int> vec;
-	int err;
-	Get_Array(Input_FName, vec, err);
-	Bubble_Sort(vec);
-	Print_Array(Output_FName, vec);
-	return 0;
+    system("color F0");
+    setlocale(LC_ALL, "russian");
+    std::vector<int> vec;
+    int err;
+    Get_Array(Input_FName, vec, err);
+    Bubble_Sort(vec);
+    Print_Array(Output_FName, vec);
+    return 0;
 }
--- TRPO1/Test_program/src/tests.cpp
+++ TRPO1/Test_program/src/tests.cpp
@@ -8,127 +8,122 @@
 #include <Windows.h>
 
 std::ostream& operator<<(std::ostream& stream, const std::vector<int>& vec) {
-	if (vec.size() == 0) {
-		return stream;
-	}
+    if (vec.size() == 0) {
+        return stream;
+    }
 
-	for (size_t i = 0; i < vec.size() - 1; i++) {
-		stream << vec.at(i) << ' ';
-	}
-	return (stream << vec.at(vec.size() - 1));
+    for (size_t i = 0; i < vec.size() - 1; i++) {
+        stream << vec.at(i) << ' ';
+    }
+    return (stream << vec.at(vec.size() - 1));
 }
 
 bool getVector(const std::string& data, std::vector<int>& vec) {
-	std::stringstream tempStream(data);
+    std::stringstream tempStream(data);
 
-	int num = 0;
+    int num = 0;
 
-	do {
-		tempStream >> num;
-		if (tempStream.fail()) {
-			return false;
-		}
-		vec.push_back(num);
-	} while (tempStream);
-	return true;
+    do {
+        tempStream >> num;
+        if (tempStream.fail()) {
+            return false;
+        }
+        vec.push_back(num);
+    } while (tempStream);
+    return true;
 }
 
 bool badFstream(std::ifstream& stream) {
-	return !stream || stream.peek() == EOF;
+    return !stream || stream.peek() == EOF;
 }
 
 void writeTest(const std::string_view file, const std::vector<int>& vec) {
-	std::ofstream output(file.data());
-	output << vec;
+    std::ofstream output(file.data());
+    output << vec;
 }
 
 bool getVectorFromFile(const std::string_view file, std::vector<int>& vec, int& errCode) {
-	std::ifstream input(file.data());
-	if (badFstream(input)) {
-		errCode = 0;
-		return false;
-	}
-	std::string temp;
-	std::getline(input, temp);
+    std::ifstream input(file.data());
+    if (badFstream(input)) {
+        errCode = 0;
+        return false;
+    }
+    std::string temp;
+    std::getline(input, temp);
 
-	if (!getVector(temp, vec)) {
-		errCode = 1;
-		return false;
-	}
+    if (!getVector(temp, vec)) {
+        errCode = 1;
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
-
 int main() {
-	const std::string inFile("Input_File.txt");
-	const std::string outFile("Output_File.txt");
-	const std::string testFile("Test_File.txt");
-	const std::string exeName("TRPO1.exe");
-	system("color F0");
+    const std::string inFile("Input_File.txt");
+    const std::string outFile("Output_File.txt");
+    const std::string testFile("Test_File.txt");
+    const std::string exeName("TRPO1.exe");
+    system("color F0");
 
-	std::vector<int> vec;
-	std::vector<int> sorted;
+    std::vector<int> vec;
+    std::vector<int> sorted;
 
-	std::ifstream testStream(testFile.data());
-	if (badFstream(testStream)) {
-		std::cout << "ERROR: Could not open file " << testFile << " or it is empty" << std::endl;
-		system("pause");
-		return 0;
-	}
-	std::string line;
-	int count = 0;
+    std::ifstream testStream(testFile.data());
+    if (badFstream(testStream)) {
+        std::cout << "ERROR: Could not open file " << testFile << " or it is empty" << std::endl;
+        system("pause");
+        return 0;
+    }
+    std::string line;
+    int count = 0;
 
-	while (!testStream.eof()) {
-		std::getline(testStream, line);
-		//std::cout << line;
-		count++;
-		int errCode = -1;
-		if (!getVector(line, vec)) {
-			std::cout << "ERROR: failed to get a test vector "
-				<< count << ": incorrect character detected." << std::endl;
-			continue;
-		}
+    while (!testStream.eof()) {
+        std::getline(testStream, line);
+        count++;
+        int errCode = -1;
+        if (!getVector(line, vec)) {
+            std::cout << "ERROR: failed to get a test vector "
+                << count << ": incorrect character detected." << std::endl;
+            continue;
+        }
 
-		writeTest(inFile, vec);
-		system(exeName.data());
+        writeTest(inFile, vec);
+        system(exeName.data());
 
-		Sleep(100);
-		if (!getVectorFromFile(outFile, sorted, errCode)) {
-			std::cout << "ERROR: ";
-			switch (errCode) {
-			case 0:
-				std::cout << "Could not open file " << outFile << " or it is empty";
-				break;
-			case 1:
-				std::cout << "detected incorrect character while reading trpo1.exe's output";
-				break;
-			default:
-				std::cout << "unpredicted error";
-				break;
-			}
-			std::cout << std::endl;
-		}
+        Sleep(100);
+        if (!getVectorFromFile(outFile, sorted, errCode)) {
+            std::cout << "ERROR: ";
+            switch (errCode) {
+            case 0:
+                std::cout << "Could not open file " << outFile << " or it is empty";
+                break;
+            case 1:
+                std::cout << "detected incorrect character while reading trpo1.exe's output";
+                break;
+            default:
+                std::cout << "unpredicted error";
+                break;
+            }
+            std::cout << std::endl;
+        }
 
-		std::sort(vec.begin(), vec.end());
-		const bool testPassed = std::equal(vec.begin(), vec.end(), sorted.begin(), sorted.end());
+        std::sort(vec.begin(), vec.end());
+        const bool testPassed = std::equal(vec.begin(), vec.end(), sorted.begin(), sorted.end());
 
-		std::cout << "Test " << count << ": ";
-		if (testPassed) {
-			std::cout << "PASSED:\n\t expected array: " << vec << "\n\t got array: " << sorted << std::endl;
-			std::cout << "\n";
-		}
-		else {
-			std::cout << "FAILED: \n\t expected array: " << vec << "\n\t got array: " << sorted << std::endl;
-			std::cout << "\n";
-		}
+        std::cout << "Test " << count << ": ";
+        if (testPassed) {
+            std::cout << "PASSED:\n\t expected array: " << vec << "\n\t got array: " << sorted << std::endl;
+            std::cout << "\n";
+        } else {
+            std::cout << "FAILED: \n\t expected array: " << vec << "\n\t got array: " << sorted << std::endl;
+            std::cout << "\n";
+        }
 
-		//line.clear();
+        vec.clear();
+        sorted.clear();
+    }
 
-		vec.clear();
-		sorted.clear();
-	}
-
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
