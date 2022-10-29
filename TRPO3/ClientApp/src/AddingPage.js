1788cf1cfd3f27175fcb4889f288871796cd75b3
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-10-29 21:58:49 +0300

    Done creating the form, had to use another multiselect

--- TRPO3/ClientApp/src/AddingPage.js
+++ TRPO3/ClientApp/src/AddingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "../src/components/Editing/editing_styles.css";
 
@@ -6,38 +6,48 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-calendar/dist/Calendar.css';
 
-import MultiSelect from 'react-multiple-select-dropdown-lite';
-import "react-multiple-select-dropdown-lite/dist/index.css";
+import Select from 'react-select';
 
 import { Link } from 'react-router-dom';
+import { callApiPost } from "./requests.js";
 
 function AddingPage() {
-
-    //---------------------------------------------
     const [form, setForm] = useState({
-        date: new Date(),
+        date: null,
         para: null,
         cabinet: null,
-        subject: null,
-        subject_type: null,
-        groups: [],
-        professors: []
+        subjectId: null,
+        subjectTypeId: null,
+        groupIds: [],
+        professorIds: []
     });
 
-    //---------------- ФЛАГИ ------------------------------
-    const [dateChosen, setDateChosen] = useState(false)
-    const [manyGroupsFlag, setManyGroupsFlag] = useState(false)
-    const [manyFiosFlag, setManyFiosFlag] = useState(false)
+    // ABSTRACT HIGH ORDER FUNCTIONS
+    const createApiLoadCall = (apiFuncName, loadingSetterFunc, fieldSetterFunc, changeDataCallback) => {
+        return (form) => {
+            loadingSetterFunc(true);
+            console.log("calling with form " + JSON.stringify(form))
+            callApiPost(apiFuncName, form, (resp) => {
+                console.log(apiFuncName + " " + JSON.stringify(resp.data))
+                fieldSetterFunc(changeDataCallback(resp.data))
+                loadingSetterFunc(false)
+            })
+        }
+    }
 
-    //----------------------------------------------
-    const [addGroups, setAddGroups] = useState([
-        { id: 1, label: "309Б", value: "М3О-309Б-20" },
-        { id: 2, label: "310Б", value: "М3О-310Б-20" }
-    ])
+    const convertToMultiselectFormat = (obj) => {
+        let label = obj.name ? obj.name : obj.fullName
+        return { label: label, value: "" + obj.id }
+    }
 
-    const createChangeHandler = (field) => {
+    const createMultiSelectChangeHandler = (field, updateFunc, isMulti) => {
         return (value) => {
-            setForm({ ...form, [field]: value })
+            console.log("changing field " + field + " to value " + JSON.stringify(value))
+            let result = isMulti ? value.map(v => v.value)
+                : (value === null ? [] : [value.value])
+            let newForm = { ...form, [field]: result }
+            setForm(newForm)
+            updateFunc(newForm)
         };
     }
 
@@ -46,25 +56,81 @@ function AddingPage() {
             setForm({ ...form, [field]: event.target.value })
         };
     }
+    // END ABSTRACT HIGH ORDER FUNCTIONS
 
-    const [addFio, setAddFio] = useState([
-        { id: 1, label: "Вестяк А.В.", value: "Вестяк Анатолий Васильевич" },
-        { id: 2, label: "Чечиков Ю.Б.", value: "Чечиков Юрий Борисович" }
-    ])
+    // DatePicker state
+    const [dateChosen, setDateChosen] = useState(false)
+    const [calendarUpdating, setCalendarUpdating] = useState(false)
+    const [disabledDates, setDisabledDates] = useState([
+        new Date("2022/10/10"),
+        new Date("2022/10/12")]);
 
-    const [addType, setAddType] = useState([
-        { id: 0, value: "Тип занятия" },
-        { id: 1, value: "Лекция" },
-        { id: 2, value: "ПЗ" },
-        { id: 3, value: "ЛР" }
+    const maximumDate = new Date("2023/07/01");
+    const minimumDate = new Date("2022/09/01");
+
+    const handleClickDay = (value) => {
+        setForm({ ...form, date: value })
+        setDateChosen(true)
+    }
+
+    useEffect(() => {
+        createApiLoadCall(
+            "GetDatesOccupied",
+            setCalendarUpdating,
+            setDisabledDates,
+            (data) => data.map(dateString => new Date(dateString))
+        )(form)
+    }, [form.groupIds, form.professorIds])
+
+    // Group multiselect state
+    const [groupsLoading, setGroupsLoading] = useState(false)
+    const [manyGroupsFlag, setManyGroupsFlag] = useState(false)
+    const [groupOptions, setGroupOptions] = useState([
+        { name: "309Б", id: 1 },
+        { name: "310Б", id: 2 }
     ])
+    const updateGroups = createApiLoadCall(
+        "GetGroupsAvailable",
+        setGroupsLoading,
+        setGroupOptions,
+        (data) => data)
+
+
+    // Fios multiselect state
+    const [manyFiosFlag, setManyFiosFlag] = useState(false)
+    const [fiosLoading, setFiosLoading] = useState(false)
+    const [fioOptions, setFioOptions] = useState([
+        { name: "Вестяк А.В.", id: "Вестяк Анатолий Васильевич" },
+        { name: "Чечиков Ю.Б.", id: "Чечиков Юрий Борисович" }
+    ])
+    const updateFios = createApiLoadCall(
+        "GetProfessorsAvailable",
+        setFiosLoading,
+        setFioOptions,
+        (data) => data)
+
+    // SubjectType select state
+    const [subjTypesLoading, setSubjTypesLoading] = useState(false)
+    const subjectTypeFirstOption = [{ id: 0, name: "Тип занятия" }]
+    const [subjectTypeOptions, setSubjectTypeOptions] = useState(
+        subjectTypeFirstOption.concat([
+            { id: 1, name: "Лекция" },
+            { id: 2, name: "Семинар" },
+            { id: 3, name: "Лаба" }
+        ]))
+    const updateSubjectTypes = createApiLoadCall(
+        "GetSubjectTypeAvailable",
+        setSubjTypesLoading,
+        setSubjectTypeOptions,
+        (data) => subjectTypeFirstOption.concat(data)
+    )
 
     const handleChangeType = (event) => {
-        setForm({ ...form, subject_type: event.target.value })
+        setForm({ ...form, subjectTypeId: event.target.value })
         let typeToGroupsAndFiosMapping = {
-            "Лекция": { manyGroups: true, manyFios: false },
-            "ЛР": { manyGroups: false, manyFios: true },
-            "ПЗ": { manyGroups: false, manyFios: false }
+            1: { manyGroups: true, manyFios: false },
+            2: { manyGroups: false, manyFios: false },
+            3: { manyGroups: false, manyFios: true }
         }
 
         let curMapping = typeToGroupsAndFiosMapping[event.target.value];
@@ -72,115 +138,179 @@ function AddingPage() {
         setManyGroupsFlag(curMapping.manyGroups);
     }
 
-    const [addPara, setAddPara] = useState([
-        { id: 0, value: "Номер пары" },
-        { id: 1, value: "Первая" },
-        { id: 2, value: "Вторая" }
-    ])
+    // Para select state
+    const paraFirstOption = [{ id: 0, name: "Номер пары" }]
+    const [paraLoading, setParaLoading] = useState(false)
+    const [paraOptions, setParaOptions] = useState(
+        paraFirstOption.concat([
+            { id: 1, name: "Первая" },
+            { id: 2, name: "Вторая" }
+        ]))
+    const updatePara = createApiLoadCall(
+        "GetParaAvailable",
+        setParaLoading,
+        setParaOptions,
+        (data) => paraFirstOption.concat(
+            data.map(
+                (para) => {
+                    return { id: para, name: para }
+                }
+            )
+        )
+    )
 
-    const [addSubj, setAddSubj] = useState([
-        { id: 0, value: "Предмет" },
-        { id: 1, value: "Матан" },
-        { id: 2, value: "ООП" }
-    ])
+    // Subject select state
+    const subjectFirstOption = [{ id: 0, name: "Предмет" }]
+    const [subjectsLoading, setSubjectsLoading] = useState(false)
+    const [subjectOptions, setSubjectOptions] = useState(
+        subjectFirstOption.concat([
+            { id: 1, name: "Матан" },
+            { id: 2, name: "ООП" }
+        ]))
 
-    const [addCab, setAddCab] = useState([
-        { id: 0, value: "Кабинет" },
-        { id: 1, value: 330 },
-        { id: 2, value: 222 }
-    ])
+    const updateSubjects = createApiLoadCall(
+        "GetSubjectsAvailable",
+        setSubjectsLoading,
+        setSubjectOptions,
+        (data) => subjectFirstOption.concat(data)
+    )
 
-    //-----------------------------------------------
+    // Cabinet select state
+    const cabinetFirstOption = [{ id: 0, name: "Кабинет" }]
+    const [cabinetsLoading, setCabinetsLoading] = useState(false)
+    const [cabinetOptions, setCabinetOptions] = useState(
+        cabinetFirstOption.concat([
+            { id: 1, name: 330 },
+            { id: 2, name: 222 }
+        ]))
+    const updateCabinet = createApiLoadCall(
+        "GetCabinetsOccupied",
+        setCabinetsLoading,
+        setCabinetOptions,
+        (data) => cabinetFirstOption.concat(
+            Array(999).fill().map(
+                (_, index) => {
+                    index += 1;
+                    return { id: index, name: index, disabled: data.includes(index) };
+                }
+            )
+        )
+    )
 
-    //КАЛЕНДАРЬ -------------------------------------
-    const maximumDate = new Date("2023/07/01");
-    const minimumDate = new Date("2022/09/01");
-
-    const [disabledDates, setDiabledDates] = useState([
-        new Date("2022/10/10"),
-        new Date("2022/10/12")]);
-
-    const handleClickDay = (value) => {
-        setForm({ ...form, date: value })
-        setDateChosen(true)
+    const formTrySubmit = () => {
+        callApiPost("PostScheduleEntryFromForm", form, (resp) => {
+            console.log("Tryed to submit the form: " + JSON.stringify(resp.data))
+        }
+        );
     }
 
-    //-----------------------------------------
     return (
         <div className="App">
+
             <h1> Это страница для добавления пар </h1>
             <label className="group_list_label"> Выбор группы </label>
-            <MultiSelect
-                className="group_list_editing" onChange={createChangeHandler("groups")} options={addGroups}
-                singleSelect={!manyGroupsFlag} />
+            <Select
+                isMulti={manyGroupsFlag}
+                isClearable={true}
+                className="group_list_editing"
+                options={groupOptions.map(convertToMultiselectFormat)}
+                onChange={createMultiSelectChangeHandler("groupIds", updateGroups, manyGroupsFlag)}
+                onFocus={() => updateGroups(form)}
+                isLoading={groupsLoading}
+            />
 
             <label className="fio_label"> Выбор преподавателя </label>
-            <MultiSelect
-                className="fio_editing" onChange={createChangeHandler("professors")} options={addFio}
-                singleSelect={!manyFiosFlag} />
+            <Select
+                isMulti={manyFiosFlag}
+                isClearable={true}
+                className="fio_editing"
+                options={fioOptions.map(convertToMultiselectFormat)}
+                onChange={createMultiSelectChangeHandler("professorIds", updateFios, manyFiosFlag)}
+                onFocus={() => updateFios(form)}
+                isLoading={fiosLoading}
+            />
 
-            <select className="type_editing" value={form.type} onChange={handleChangeType}>
-                {addType.map(subj_type =>
-                    <option key={subj_type.id} value={subj_type.value}>{subj_type.value}</option>)}
+            <select className="type_editing"
+                value={form.type}
+                onChange={handleChangeType}
+                onClick={() => updateSubjectTypes(form)}
+            >
+                {subjectTypeOptions.map(subj_type =>
+                    <option disabled={subjTypesLoading} key={subj_type.id}
+                        value={subj_type.id}>{subj_type.name}</option>)}
             </select>
-
 
             <div className="calendar">
                 <DatePicker
-                    selected={form.date}
+                    selected={dateChosen ? form.date : new Date()}
                     onChange={handleClickDay}
                     minDate={minimumDate}
                     // Disable Datepicker
-                    maxDate={(form.groups.length === 0 && form.professors.length === 0)
+                    maxDate={(form.groupIds.length === 0 && form.professorIds.length === 0)
+                        || calendarUpdating
                         ? minimumDate : maximumDate}
                     excludeDates={disabledDates}
                     inline
                 />
             </div>
 
-
             <div>
-                <select disabled={!dateChosen} className="para" value={form.para} onChange={createEventChangeHandler("para")}>
-                    {addPara.map(addingPara =>
-                        <option key={addingPara.id} value={addingPara.value}>{addingPara.value}</option>)}
+                <select className="para"
+                    disabled={!dateChosen}
+                    value={form.para}
+                    onChange={createEventChangeHandler("para")}
+                    onClick={() => updatePara(form)}
+                >
+                    {paraOptions.map(addingPara =>
+                        <option disabled={paraLoading} key={addingPara.id}
+                            value={addingPara.id}>{addingPara.name}</option>)}
                 </select>
 
                 <select
                     disabled={
-                        (form.groups.length === 0 && form.professors.length === 0)
+                        (form.groupIds.length === 0 && form.professorIds.length === 0)
                     }
                     className="subj_editing"
                     value={form.subj}
-                    onChange={createEventChangeHandler("subject")}
+                    onChange={createEventChangeHandler("subjectId")}
+                    onClick={() => updateSubjects(form)}
                 >
-                    {addSubj.map(addingSubj => <option key={addingSubj.id}
-                        value={addingSubj.value}>{addingSubj.value} </option>)}
+                    {subjectOptions.map(addingSubj => <option disabled={subjectsLoading} key={addingSubj.id}
+                        value={addingSubj.id}>{addingSubj.name} </option>)}
                 </select>
 
-                <select disabled={form.subject == null}
+                <select disabled={form.subjectId == null}
                     className="cabinet_editing"
                     value={form.cabinet}
                     onChange={createEventChangeHandler("cabinet")}
+                    onClick={() => updateCabinet(form)}
                 >
-                    {addCab.map(addingCab => <option key={addingCab.id}
-                        value={addingCab.value}> {addingCab.value}</option>)}
+                    {cabinetOptions.map(addingCab => <option key={addingCab.id}
+                        disabled={cabinetsLoading || addingCab.disabled}
+                        value={addingCab.value}> {addingCab.name}</option>)}
                 </select>
 
             </div>
-
             {
                 <div>
-                    <Link to="/Schedule"><button disabled={true} className="Approve_button"> Подтвердить </button></Link>
-                    <Link to="/" state={{ from: "AddingPage", update: true }}><button className="Exit_button"> Главное меню </button></Link>
+                    <Link to="/">
+                        <button disabled={false}
+                            className="Approve_button"
+                            onClick={formTrySubmit}>
+                            Подтвердить
+                        </button>
+                    </Link>
+
+                    <Link to="/" state={{ from: "AddingPage", update: true }}>
+                        <button className="Exit_button">
+                            Главное меню
+                        </button>
+                    </Link>
 
                 </div>
             }
-
-
         </div>
-
     );
-
 }
 
 export default AddingPage;
