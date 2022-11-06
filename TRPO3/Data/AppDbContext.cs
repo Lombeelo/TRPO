67822556dee723fa1c79b04fc7fd7bcb79729d1b
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-11-06 18:39:17 +0300

    Filled database with real data (no schedule entries provided)

--- TRPO3/Data/AppDbContext.cs
+++ TRPO3/Data/AppDbContext.cs
@@ -47,22 +47,77 @@ public sealed class AppDbContext : DbContext
             new Subject
             {
                 Id = 1,
-                Name = "Теория графов"
-            },
-            new Subject
-            {
-                Id = 2,
-                Name = "САПР"
-            },
-            new Subject
-            {
-                Id = 3,
                 Name = "ТРПО"
             },
             new Subject
             {
+                Id = 2,
+                Name = "Теория автоматического управления"
+            },
+            new Subject
+            {
+                Id = 3,
+                Name = "Теория графов"
+            },
+            new Subject
+            {
                 Id = 4,
-                Name = "Математический анализ"
+                Name = "Экология"
+            },
+            new Subject
+            {
+                Id = 5,
+                Name = "Метрология, стандартизация и сертификация"
+            },
+            new Subject
+            {
+                Id = 6,
+                Name = "Аналитическое моделирование"
+            },
+            new Subject
+            {
+                Id = 7,
+                Name = "Архитектуры вычислительных систем"
+            },
+            new Subject
+            {
+                Id = 8,
+                Name = "Основы менеджмента"
+            },
+            new Subject
+            {
+                Id = 9,
+                Name = "Автоматизация проектирования"
+            },
+            new Subject
+            {
+                Id = 10,
+                Name = "Эргономика и интерактивные системы АСОИУ"
+            },
+            new Subject
+            {
+                Id = 11,
+                Name = "Базы данных"
+            },
+            new Subject
+            {
+                Id = 12,
+                Name = "Интерфейсы периферийных устройств"
+            },
+            new Subject
+            {
+                Id = 13,
+                Name = "Математическое программирование"
+            },
+            new Subject
+            {
+                Id = 14,
+                Name = "Основы теории управления"
+            },
+            new Subject
+            {
+                Id = 15,
+                Name = "Планирование эксперимента"
             },
         };
         var ltypes = new List<LessonType>
@@ -84,87 +139,180 @@ public sealed class AppDbContext : DbContext
             },
         };
 
-        var groups = new List<Group> {
-            new Group
-            {
+        var groups = new List<Group>
+        {
+            new Group {
                 Id = 1,
-                Name = "М3О-309Б-20",
-                Subjects = subjects
-            },
-            new Group
-            {
-                Id = 2,
                 Name = "М3О-307Б-20",
-                Subjects = subjects.Skip(2).ToList()
-            },
-            new Group
-            {
+                Subjects = new List<Subject> {
+                    subjects[0],
+                    subjects[1],
+                    subjects[3],
+                    subjects[4],
+                    subjects[5],
+                    subjects[6],
+                    subjects[7],
+                    subjects[8],
+                    subjects[10],
+                    subjects[14],
+            } },
+            new Group {
+                Id = 2,
+                Name = "М3О-309Б-20",
+                Subjects = new List<Subject> {
+                    subjects[0],
+                    subjects[1],
+                    subjects[3],
+                    subjects[4],
+                    subjects[5],
+                    subjects[6],
+                    subjects[7],
+                    subjects[8],
+                    subjects[2],
+                    subjects[9],
+                    subjects[11],
+            } },
+            new Group {
                 Id = 3,
                 Name = "М3О-310Б-20",
-                Subjects = subjects.Take(2).ToList()
-            }
+                Subjects = new List<Subject> {
+                    subjects[0],
+                    subjects[1],
+                    subjects[3],
+                    subjects[4],
+                    subjects[5],
+                    subjects[6],
+                    subjects[7],
+                    subjects[8],
+                    subjects[12],
+                    subjects[13],
+            } },
         };
         var profs = new List<Professor>
         {
-            new Professor
-            {
+            new Professor {
                 Id = 1,
-                Name = "Ратников М.О.",
-                Subjects = subjects.Skip(1).Take(2).ToList() // 2,3
-            },
-            new Professor
-            {
+                Name = "Татарникова Елена Михайловна",
+                Subjects = new List<Subject> {
+                    subjects[0],
+            } },
+            new Professor {
                 Id = 2,
-                Name = "Чугаев Б.Н.",
-                Subjects = subjects.Take(1).ToList() // 1
-            },
-            new Professor
-            {
+                Name = "Прокимнова Татьяна Геннадьевна",
+                Subjects = new List<Subject> {
+                    subjects[0],
+            } },
+            new Professor {
                 Id = 3,
-                Name = "Вестяк А.В.",
-                Subjects = subjects.Skip(3).ToList() // 4
-            }
-
+                Name = "Мурсенков Павел Александрович",
+                Subjects = new List<Subject> {
+                    subjects[11],
+                    subjects[5],
+                    subjects[8],
+            } },
+            new Professor {
+                Id = 4,
+                Name = "Ткачёв Олег Алексеевич",
+                Subjects = new List<Subject> {
+                    subjects[8],
+            } },
+            new Professor {
+                Id = 5,
+                Name = "Брехов Олег Михайлович",
+                Subjects = new List<Subject> {
+                    subjects[5],
+                    subjects[6],
+            } },
+            new Professor {
+                Id = 6,
+                Name = "Звонарёва Галина Александровна",
+                Subjects = new List<Subject> {
+                    subjects[6],
+            } },
+            new Professor {
+                Id = 7,
+                Name = "Макаренкова Надежда Алексеевна",
+                Subjects = new List<Subject> {
+                    subjects[1],
+            } },
+            new Professor {
+                Id = 8,
+                Name = "Романов Олег Тимофеевич",
+                Subjects = new List<Subject> {
+                    subjects[2],
+            } },
+            new Professor {
+                Id = 9,
+                Name = "Чечиков Юрий Борисович",
+                Subjects = new List<Subject> {
+                    subjects[10],
+            } },
+            new Professor {
+                Id = 10,
+                Name = "Сергеева Ирина Анатольевна",
+                Subjects = new List<Subject> {
+                    subjects[4],
+            } },
+            new Professor {
+                Id = 11,
+                Name = "Титов Юрий Павлович",
+                Subjects = new List<Subject> {
+                    subjects[14],
+            } },
+            new Professor {
+                Id = 12,
+                Name = "Давыдкина Елена Александровна",
+                Subjects = new List<Subject> {
+                    subjects[14],
+            } },
+            new Professor {
+                Id = 13,
+                Name = "Сотникова Елена Васильевна",
+                Subjects = new List<Subject> {
+                    subjects[3],
+            } },
+            new Professor {
+                Id = 14,
+                Name = "Гусилетов Александр Андреевич",
+                Subjects = new List<Subject> {
+                    subjects[3],
+            } },
+            new Professor {
+                Id = 15,
+                Name = "Ратников Максим Олегович",
+                Subjects = new List<Subject> {
+                    subjects[9],
+            } },
+            new Professor {
+                Id = 16,
+                Name = "Григоревский Николай Владимирович",
+                Subjects = new List<Subject> {
+                    subjects[12],
+            } },
+            new Professor {
+                Id = 17,
+                Name = "Кудрявцев Павел Сергеевич",
+                Subjects = new List<Subject> {
+                    subjects[13],
+            } },
+            new Professor {
+                Id = 18,
+                Name = "Костыгова Людмила Александровна",
+                Subjects = new List<Subject> {
+                    subjects[7],
+            } },
+            new Professor {
+                Id = 19,
+                Name = "Латыпов Эдгар Рашитович",
+                Subjects = new List<Subject> {
+                    subjects[7],
+            } },
         };
 
-        Groups.AddRange(groups);
         Subjects.AddRange(subjects);
-        Professors.AddRange(profs);
         LessonTypes.AddRange(ltypes);
-
-        Schedule.AddRange(new List<ScheduleEntry> {
-            new ScheduleEntry {
-                Id = 1,
-                Date = DateTime.Today,
-                Cabinet = 304,
-                Para = 2,
-                Groups = new List<Group> { groups[0] },
-                Professors = new List<Professor> { profs[0], profs[1] },
-                Subject = subjects[0],
-                Type = ltypes[2]
-            },
-            new ScheduleEntry {
-                Id = 2,
-                Date = DateTime.Today.AddDays(-1),
-                Cabinet = 504,
-                Para = 3,
-                Subject = subjects.Last(),
-                Groups = groups.Take(2).ToList(),
-                Professors = profs.Skip(2).ToList(),
-                Type = ltypes[0]
-            },
-            new ScheduleEntry {
-                Id = 3,
-                Date = DateTime.Today.AddDays(1),
-                Cabinet = 108,
-                Para = 3,
-                Subject = subjects.Skip(1).Take(1).ToArray()[0],
-                Groups = new List<Group> {groups[0]},
-                Professors = new List<Professor> {profs[1]},
-                Type = ltypes[1]
-            }
-        });
-
+        Groups.AddRange(groups);
+        Professors.AddRange(profs);
         SaveChanges();
     }
 }
